cd9c4657ac7f1b42dafa18e60e4045e058f24bdf
Dmitry029 <kdp.029029@gmail.com>
2021-05-13 22:32:40 +0300

    local grid is done

--- src/main/java/com/practicetestautomation/base/BaseTest.java
+++ src/main/java/com/practicetestautomation/base/BaseTest.java
@@ -1,7 +1,5 @@
 package com.practicetestautomation.base;
 
-import java.lang.reflect.Method;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
@@ -11,24 +9,43 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.lang.reflect.Method;
+import java.time.Duration;
+
+import static com.practicetestautomation.base.BrowserDriverFactory.createDriver;
+
 public class BaseTest {
 
-	protected WebDriver driver;
-	protected Logger log;
+    protected WebDriver driver;
+    protected Logger log;
 
-	@Parameters({ "browser" })
-	@BeforeMethod(alwaysRun = true)
-	public void setUp(Method method, @Optional("chrome") String browser, ITestContext ctx) {
-		log = LogManager.getLogger(ctx.getCurrentXmlTest().getSuite().getName());
-		driver = new BrowserDriverFactory(browser, log).createDriver();
-		driver.manage().window().maximize();
-	}
+    @Parameters({"browser", "environment", "platform"})
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(Method method,
+            @Optional("chrome") String browser,
+            @Optional("local") String environment,
+            @Optional("WIN10") String platform,
+            ITestContext ctx) {
+        log = LogManager.getLogger(ctx.getCurrentXmlTest().getSuite().getName());
 
+        switch (environment) {
+            case "local":
+                driver = createDriver(browser, log);
+                break;
+            case "grid":
+                driver = new GridFactory(browser, platform, log).createDriver();
+                break;
+            default:
+                driver = createDriver(browser, log);
+        }
 
-	@AfterMethod(alwaysRun = true)
-	public void tearDown() {
-		log.info("Close driver");
-		driver.quit();
-	}
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
 
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        log.info("Close driver");
+        driver.quit();
+    }
 }
